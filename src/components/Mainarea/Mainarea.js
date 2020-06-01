907b0702e7e72cbdc1080445599945a874e4140b
Abhinav Gunishety <ag513@student.le.ac.uk>
2020-06-01 22:22:03 +0100

    added left nav links

--- src/components/Mainarea/Mainarea.js
+++ src/components/Mainarea/Mainarea.js
@@ -3,16 +3,52 @@ import { Container, Row, Col, Button } from 'reactstrap';
 import TodoModal from '../Todo/TodoModal/todomodal';
 import TodoCard from '../Todo/TodoCard/TodoCard';
 import Pomodoro from '../Pomodoro/pomodoro';
+import { Collapse, CardBody, Card } from 'reactstrap';
+import {PriorityFlag} from '../Todo/TodoModal/todomodal';
 
 const MainArea = (props) => {
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle1 = () => setIsOpen(!isOpen);
+  const toggle2 = () => setIsOpen(!isOpen);
   return (
     <Container fluid>
       <Row className='main-layout-row'>
         <Col sm="2" md="2" className="labels-menu lebels-menu--left">
-
+          <div className="left-nav">
+            <div className="left-nav__body">
+              <Button color="secondary">Inbox</Button>{' '}
+              <Button color="secondary">Completed</Button>{' '}
+              <div className="button">
+                <Button color="primary" onClick={toggle1} style={{ marginBottom: '1rem' }}>Labels</Button>
+                <Collapse isOpen={isOpen}>
+                  <Card>
+                    <Button color="secondary">label 1</Button>{' '}
+                    <Button color="secondary">label 2</Button>{' '}
+                    <Button color="secondary">label 3</Button>{' '}
+                    <Button color="secondary">label 4</Button>{' '}
+                    <Button color="secondary">label 5</Button>{' '}
+                    <Button color="secondary">label 6</Button>{' '}
+                    <Button color="secondary">label 7</Button>{' '}
+                    <Button color="secondary">label 8</Button>{' '}
+                    <Button color="secondary">label 9</Button>{' '}
+                    <Button color="secondary">label 10</Button>{' '}
+                  </Card>
+                </Collapse>
+              </div>
+              <div className="button">
+                <Button color="primary" onClick={toggle2} style={{ marginBottom: '1rem' }}>Priority</Button>
+                <Collapse isOpen={isOpen}>
+                  <Card>
+                    <Button color="secondary">Priority 1 <PriorityFlag className="left-nav__priority-flag" fill="#b23b47"  /></Button>{' '}
+                    <Button color="secondary">Priority 2 <PriorityFlag className="left-nav__priority-flag" fill="green"  /></Button>{' '}
+                    <Button color="secondary">Priority 3 <PriorityFlag className="left-nav__priority-flag" fill="orange"  /></Button>{' '}
+                  </Card>
+                </Collapse>
+              </div>
+            </div>
+          </div>
         </Col>
         <Col className="todo-main-section" sm="7" md="7">
           <div className="todo-section-header">
